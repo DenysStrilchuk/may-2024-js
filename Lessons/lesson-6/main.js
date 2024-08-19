3ed67d27389dedf671c7fb06562d105b5fdea26c
Denys Sttrilchuk <denia876@gmail.com>
2024-08-19 15:45:47 +0300

    Lesson-6(array_functions)

--- Lessons/lesson-6/main.js
+++ Lessons/lesson-6/main.js
@@ -3,6 +3,7 @@
 - Знайти та вивести довижину настипних стрінгових значень*!/
 'hello world', 'lorem ipsum', 'javascript is cool'*/
 
+
 const helloWorld = 'hello world';
 const loremIpsum = 'lorem ipsum';
 const javascriptIsCool = 'javascript is cool';
